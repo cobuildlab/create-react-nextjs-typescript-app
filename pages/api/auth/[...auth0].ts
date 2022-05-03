ca76e991d71d4b65ac3094a5d7586a26b24a43f0
ManueAC <manuelacp0071@gmail.com>
2022-05-03 19:10:46 -0400

    Add auth0 setup

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -1,3 +1,24 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { auth0 } from '../../../src/utils/auth';
 
-export default handleAuth();
+export default auth0.handleAuth({
+  async callback(req, res) {
+    try {
+      await auth0.handleCallback(req, res, {
+        // eslint-disable-next-line
+        afterCallback: async (req, res, session, state) => {
+          const { idToken, user } = session;
+          console.log('handleAuth:', {
+            idToken: idToken,
+            user: user,
+          });
+          //   await handleAuthentication(idToken, user);
+          return {
+            ...session,
+          };
+        },
+      });
+    } catch (error) {
+      res.status(error.status || 500).end(error.message);
+    }
+  },
+});
