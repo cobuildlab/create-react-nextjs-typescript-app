3bcebb915a79b27936b8a041b8743495833f7354
ManueAC <manuelacp0071@gmail.com>
2022-05-04 08:13:53 -0400

    restore auth setup

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -1,24 +1,3 @@
-import { auth0 } from '../../../src/utils/auth';
+import { handleAuth } from '@auth0/nextjs-auth0';
 
-export default auth0.handleAuth({
-  async callback(req, res) {
-    try {
-      await auth0.handleCallback(req, res, {
-        // eslint-disable-next-line
-        afterCallback: async (req, res, session, state) => {
-          const { idToken, user } = session;
-          console.log('handleAuth:', {
-            idToken: idToken,
-            user: user,
-          });
-          //   await handleAuthentication(idToken, user);
-          return {
-            ...session,
-          };
-        },
-      });
-    } catch (error) {
-      res.status(error.status || 500).end(error.message);
-    }
-  },
-});
+export default handleAuth();
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,18 +5,11 @@ export const WORKSPACE_ENDPOINT = `https://api.8base.com/${WORKSPACE_ID}${
     ? ''
     : `_${ENVIRONMENT_NAME}`
 }`;
-export const AUTH_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID || '';
-export const AUTH_CLIENT_SECRET = process.env.REACT_APP_AUTH0_CLIENT_SECRET || '';
-export const AUTH_BASE_URL = process.env.REACT_APP_AUTH0_BASE_URL || '';
-export const AUTH_CLIENT_DOMAIN = process.env.REACT_APP_AUTH0_CLIENT_DOMAIN || '';
-export const AUTH_SCOPE = process.env.REACT_APP_AUTH0_SCOPE || '';
-export const AUTH_SECRET = process.env.REACT_APP_AUTH0_SECRET || '';
-
 export const AUTH_PROFILE_ID = process.env.REACT_APP_AUTH0_PROFILE_ID;
+export const AUTH_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID || '';
+export const AUTH_CLIENT_DOMAIN = process.env.REACT_APP_AUTH0_CLIENT_DOMAIN || '';
 export const EIGHTBASE_WS_ENDPOINT = 'wss://ws.8base.com';
 export const AUTH_CLIENT_REDIRECT_URI = '';
 export const AUTH_CLIENT_REDIRECT_LOGOUT = '';
 // export const AUTH_CLIENT_REDIRECT_URI = (typeof window.location.origin != 'undefined')?`${window.location.origin}/auth/callback`:'';
 // export const AUTH_CLIENT_REDIRECT_LOGOUT = (typeof window.location.origin != 'undefined')?`${window.location.origin}/logout`:'';
-
-
