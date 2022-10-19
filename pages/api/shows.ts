dc26a98013ea5016b7f9c21107c5f522d9dbbd64
Leonel Lima <leonellimap@gmail.com>
2022-10-19 11:36:15 -0400

    Fix error

--- pages/api/shows.ts
+++ pages/api/shows.ts
@@ -15,6 +15,7 @@ export default withApiAuthRequired(async function shows(req, res) {
 
     res.status(200).json(show);
   } catch (error) {
-    res.status(error.status || 500).json({ error: error.message });
+    // res.status(error.status || 500).json({ error: error.message });
+    res.status(500).json({ error: ''});
   }
 });
